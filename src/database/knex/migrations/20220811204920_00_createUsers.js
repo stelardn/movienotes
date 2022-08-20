6e4e798cefd60e9acead091e5eddda39cf5d6eca
stéfany <stelar.dev@gmail.com>
2022-08-20 15:31:11 -0300

    movie notes migration

--- src/database/knex/migrations/20220811204920_00_createUsers.js
+++ src/database/knex/migrations/20220811204920_00_createUsers.js
@@ -1,9 +1,9 @@
 exports.up = knex => knex.schema.createTable("users", table => {
-  table.increments("id");
+  table.increments("id"); //primary
   table.text("name");
   table.text("email");
   table.text("password");
-  table.text("avatar").nullable();
+  table.text("avatar");
   table.timestamp("created_at").default(knex.fn.now());
   table.timestamp("updated_at").default(knex.fn.now());
 });
