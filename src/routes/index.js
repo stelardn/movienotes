d12412f9b6517dc22969b20d44565740b68da4a5
stéfany <stelar.dev@gmail.com>
2022-10-04 19:10:06 -0300

    create session with token ok

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,6 +1,7 @@
 const userRouter = require("./user.routes");
 const movieNotesRouter = require("./movieNotes.routes");
 const movieTagsRouter = require("./movieTags.routes");
+const sessionsRouter = require("./sessions.routes");
 
 const { Router } = require("express");
 
@@ -9,5 +10,6 @@ const routes = Router();
 routes.use('/users', userRouter);
 routes.use('/movie_notes', movieNotesRouter);
 routes.use('/movie_tags', movieTagsRouter);
+routes.use('/sessions', sessionsRouter);
 
 module.exports = routes;
